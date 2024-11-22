30aba4d69f31705f5a84c9d2618852ea0fbb6004
JuliaShubina <JuliaShakir@gmail.com>
2024-11-22 10:07:47 +0200

    start switching the site (auto-gbo-service) to React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { MyComponent } from "./components/MyComponent";
+import "./scss/main.scss";
+
+function App() {
+  return (
+    <>
+      <MyComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,5 @@
+const MyComponent = () => {
+  return <div>hello,world</div>;
+};
+
+export { MyComponent };
