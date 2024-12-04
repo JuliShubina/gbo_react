cf268bdb95a5c5a95aed8c66293d12efc8dd1b8a
JuliaShubina <JuliaShakir@gmail.com>
2024-12-04 15:42:02 +0200

    add scroll (like hero_icon)

--- src/components/hero icon/HeroIcon.jsx
+++ src/components/hero icon/HeroIcon.jsx
@@ -4,11 +4,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./hero__icon.css";
 
 const HeroIcon = () => {
+  const scrollToContent = (sectionId) => {
+    const element = document.getElementById(sectionId); // Find the element with the ID
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" }); // Smooth scrolling
+    } else {
+      console.warn("Element with ID", sectionId, "not found");
+    }
+  };
+
   return (
     <div className="hero__content-icon">
-      <FontAwesomeIcon icon={faChevronDown} />
+      <FontAwesomeIcon id="FontAwesomeIcon" onClick={() => scrollToContent("FontAwesomeIcon")} icon={faChevronDown} />
     </div>
   );
 };
 
 export default HeroIcon;
+
+// const arrowDown = document.querySelector(".hero__content-icon");
+
+// arrowDown.addEventListener("click", () => {
+//   const element = document.querySelector(".hero__content-icon");
+
+//   element.scrollIntoView({
+//     behavior: "smooth",
+//   });
+// });
