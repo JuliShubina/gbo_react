48b7173207829a9ccdaa751664cc79da69f9cb20
JuliaShubina <JuliaShakir@gmail.com>
2024-12-04 15:40:54 +0200

    add media query (like hero_icon_visibility)

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,16 +1,20 @@
-import React from "react";
 import styles from "./hero.module.css";
 import HeroIcon from "../../components/hero icon/HeroIcon";
 import PropTypes from "prop-types";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = (props) => {
+  const isLandscapeSmallHeight = useMediaQuery({
+    query: "(max-height: 500px) and (orientation: landscape)",
+  });
+
   return (
     <section className={`${styles.hero} ${styles.intersection} ${styles.hero__background}`}>
       {/* <section className="hero intersection hero__background"> */}
       <div className="container">
         <div className={styles.hero__content}>
           <h1 className={styles.hero__contentTitle}>{props.title}</h1>
-          <HeroIcon />
+          {isLandscapeSmallHeight && <HeroIcon />}
         </div>
       </div>
     </section>
