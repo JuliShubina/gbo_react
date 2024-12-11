931ba2ae87e4db84b79a9566d573a51ae8188e22
JuliaShubina <JuliaShakir@gmail.com>
2024-12-11 14:42:29 +0200

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/gbo_react",
+  base: "/gbo_react/",
   plugins: [react()],
   server: {
     port: 3000,
