7786be51ce9c6831fc4a94af829ca2a6fd8bcb79
JuliaShubina <JuliaShakir@gmail.com>
2025-01-20 15:11:41 +0200

    add dropdown menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,44 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import "./header.css";
 import "./burger.css";
 import { NavLink } from "react-router-dom";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
-
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
   const closeMenu = () => {
     setMenuOpen(false);
+    setIsDropdownOpen(false);
   };
 
   return (
-    <header className={`header ${menuOpen ? "open" : ""}`}>
+    <header className={`header ${menuOpen ? "open" : ""} ${isScrolled ? "scrolled" : ""}`}>
       <div className="header__inner">
         <button onClick={toggleMenu} className="header__burger-btn" id="burger">
           <span></span>
@@ -23,7 +46,7 @@ const Header = () => {
           <span></span>
         </button>
         <NavLink to="/" className="logo">
-          auto-gbo-service
+          car-service
         </NavLink>
 
         <nav className="menu">
@@ -33,11 +56,35 @@ const Header = () => {
                 Головна
               </NavLink>
             </li>
-            <li className="menu__list-item">
-              <NavLink onClick={closeMenu} className="menu__list-link" to="/services">
-                Послуги
-              </NavLink>
+
+            <li className="menu__list-item menu__dropdown">
+              <div className="only-style">
+                <span className="menu__list-link" onClick={toggleDropdown}>
+                  Послуги
+                </span>
+                <FontAwesomeIcon onClick={toggleDropdown} className="dropdown_icon" id="FontAwesomeIcon" icon={faChevronDown} />
+              </div>
+              {isDropdownOpen && (
+                <ul className={`menu__dropdown-list ${isDropdownOpen ? "open" : ""}`}>
+                  <li className="menu__dropdown-item">
+                    <NavLink onClick={closeMenu} to="/services/gbo" className="menu__list-link">
+                      ГБО
+                    </NavLink>
+                  </li>
+                  <li className="menu__dropdown-item">
+                    <NavLink onClick={closeMenu} to="/services/autoelectric" className="menu__list-link">
+                      Автоелектрик
+                    </NavLink>
+                  </li>
+                  <li className="menu__dropdown-item">
+                    <NavLink onClick={closeMenu} to="/services/cooling-system" className="menu__list-link">
+                      Система охолодження
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
             </li>
+
             <li className="menu__list-item">
               <NavLink onClick={closeMenu} className="menu__list-link" to="/about">
                 Про нас
