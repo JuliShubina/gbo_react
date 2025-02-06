1562df72f9284865ca6d9d91fd756bd288c203d8
JuliaShubina <JuliaShakir@gmail.com>
2025-02-06 16:14:06 +0200

    deleted basename props

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/gbo_react">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
